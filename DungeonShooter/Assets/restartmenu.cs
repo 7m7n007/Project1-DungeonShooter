aadc3bda4ee4aaaeb6ce47aaed5a175c737beec2
ShayD12 <lakshya766711@gmail.com>
2023-06-11 22:16:07 +0530

    complete game

--- DungeonShooter/Assets/restartmenu.cs
+++ DungeonShooter/Assets/restartmenu.cs
@@ -5,11 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class restartmenu : MonoBehaviour
 {
-    public void restartgame(){
+    public void restartgame2(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex-1);
     }
+<<<<<<< Updated upstream
     public void quitbutton(){
         Debug.Log("QUIT!");
         Application.Quit();
+=======
+    public void quitbutton2(){
+        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex-2);
+>>>>>>> Stashed changes
     }
 }
